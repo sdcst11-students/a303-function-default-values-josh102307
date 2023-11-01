5678d083137f376eee7bfc53c3c2cb399525c86f
josh102307 <144732172+josh102307@users.noreply.github.com>
2023-11-01 13:18:48 -0700

    i did the tasks

--- task1.py
+++ task1.py
@@ -22,7 +22,8 @@ assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Hav
 (2 points) 
 """
 
-def sentence():
+def sentence(greeting="hello", name="benjamin", question="how are you"):
+  print(sentence)
   return ""
 
 
